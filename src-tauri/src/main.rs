27ecdec5b84e8ad7b458bc0ca7e563f808b9c1bb
rafael <rrrr0ffff@gmail.com>
2024-07-13 01:59:58 -0300

    error handling       kinda

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,14 +9,33 @@ mod models;
 
 #[tauri::command]
 fn get_data(city: &str) -> Response {
-    reqwest::blocking::get(format!(
+    let response = reqwest::blocking::get(format!(
         "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric",
         city,
         env::var("WEATHER_API_KEY").expect("Error reading the API key")
-    ))
-    .unwrap()
-    .json::<Response>()
-    .unwrap()
+    ));
+    match response {
+        Ok(r) => {
+            let Response {
+                coord,
+                main,
+                name,
+                timezone,
+                visibility,
+                wind,
+            } = r.json().unwrap();
+
+            Response {
+                coord,
+                main,
+                name,
+                timezone,
+                visibility,
+                wind,
+            }
+        }
+        Err(_e) => todo!(),
+    }
 }
 
 fn main() {
